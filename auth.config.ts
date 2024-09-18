9742d19cf1b08f0a10772d17df42d2fba53946b3
Vinay Shetye <vinay.courses.official@gmail.com>
2024-09-17 23:47:38 -0400

    updated files

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import NextAuth from "next-auth"
 import type { NextAuthConfig } from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
@@ -16,22 +15,3 @@ export const authConfig = {
 export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
 
 
-=======
-import NextAuth from "next-auth"
-import type { NextAuthConfig } from 'next-auth';
-import GoogleProvider from "next-auth/providers/google";
-
-
-export const authConfig = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ],
-} satisfies NextAuthConfig;
-
-export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
-
-
->>>>>>> 5c64806be0cf1150f7df0a62bd2f159fe0783a15
